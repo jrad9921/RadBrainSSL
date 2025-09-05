b203443013dabc8ad4dc4e1c0490aabeb35c2aff
jrad9921 <107748738+jrad9921@users.noreply.github.com>
2025-09-05 13:59:55 +0200

    Update train_feats_cls.py

--- trainers/train_feats_cls.py
+++ trainers/train_feats_cls.py
@@ -7,20 +7,18 @@ from sklearn.metrics import roc_auc_score, accuracy_score
 import joblib  # for saving model
 
 # === Config ===
-label = 'sex'  # or 'ftd', 'pd'
-cohort = 'ukb'
+label = 'label'  # or 'ftd', 'pd'
+cohort = 'mspaths'
 model = 'sfcne'
-csv = 'demographics'
+csv = 'ms-cn'
 feature_csv = f'../../features/{cohort}/{model}/_features.csv'
-
 train_csv = f'../../data/{cohort}/train/{csv}.csv'
-val_csv   = f'../../data/{cohort}/val/{csv}.csv'
 test_csv  = f'../../data/{cohort}/test/{csv}.csv'
 #%%
 # Output path for saved model
 model_dir = f'../../models/{model}'
 os.makedirs(model_dir, exist_ok=True)
-model_path = f'{model_dir}/{label}_{cohort}.joblib'
+model_path = f'{model_dir}/{csv}-{cohort}.joblib'
 
 # === Load features
 df_features = pd.read_csv(feature_csv)
@@ -28,22 +26,19 @@ feature_cols = [col for col in df_features.columns if col.isdigit()]
 
 # Load splits
 df_train = pd.read_csv(train_csv)
-df_val   = pd.read_csv(val_csv)
 df_test  = pd.read_csv(test_csv)
 
 # Standardize EID column name
-for df in [df_train, df_val, df_test]:
+for df in [df_train, df_test]:
     if 'eid__sequence' in df.columns:
         df.rename(columns={'eid__sequence': 'eid'}, inplace=True)
 
 # Merge features with each split
 df_train = df_train.merge(df_features, on='eid', how='inner')
-df_val   = df_val.merge(df_features, on='eid', how='inner')
 df_test  = df_test.merge(df_features, on='eid', how='inner')
 
 # Extract X and y
 X_train, y_train = df_train[feature_cols].values, df_train[label].values
-X_val,   y_val   = df_val[feature_cols].values,   df_val[label].values
 X_test,  y_test  = df_test[feature_cols].values,  df_test[label].values
 
 # === Train linear classifier
@@ -61,7 +56,7 @@ def evaluate(name, X, y):
     acc = accuracy_score(y, y_pred > 0.5)
     print(f"{name} — AUROC: {auroc:.4f}, ACC: {acc:.4f}")
 
-evaluate("✅ Val", X_val, y_val)
 evaluate("✅ Test", X_test, y_test)
 
 # %%
+
